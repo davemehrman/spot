b2f6a959188ba3d7cbab82c308872c3403b12d72
davemehrman <dmehrman@gmail.com>
2012-02-25 20:34:57 -0600

    add auto login after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        session[:uid] = @user.id
+        format.html { redirect_to spots_url, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: "new" }
